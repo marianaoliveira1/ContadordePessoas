ca1b136da26c8c9c6f7daebe52ed09344d434497
Mariana Oliveira <marianaoliveirafernandes19@gmail.com>
2021-10-05 19:29:19 -0300

    up

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,24 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: "Contador de Pessoas",
-    home: Container(color: Colors.white,),
-  ));
+      title: "Contador de Pessoas",
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Pessoas: 0",
+            style: TextStyle(color: Colors.white,
+                fontWeight: FontWeight.bold),
+          ),
+          Text(
+            "Pode entrar!",
+            style: TextStyle(color: Colors.white,
+                fontStyle: FontStyle.italic,
+                fontSize: 30.0
+            ),
+          ),
+        ],
+      )));
 }
